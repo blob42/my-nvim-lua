ddb78b21d0a1e593ac13e400a4306b63443337cc
spike <spike@w530>
2022-09-11 13:48:01 +0200

    marks.nvim config file (marks is unused now)

---
+++ lua/custom/plugins/configs/marks.lua
@@ -0,0 +1,23 @@
+local present, marks = pcall(require, "marks")
+
+if not present then
+  return
+end
+
+
+M = {}
+
+local config = {
+  bookmark_0 = {
+        sign= "",
+        virt_text = "vim",
+        annotate = true,
+  }
+
+}
+
+M.setup = function()
+  marks.setup(config)
+end
+
+return M
