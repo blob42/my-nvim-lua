465dbe3de7e67fcc9b14e46921ba21b952e8d015
blob42 <spike@w530>
2023-01-30 22:00:16 +0100

    working chatgpt w/ lazyload

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -143,14 +143,21 @@ return {
             require("core.utils").load_mappings "copilot"
         end
     },
+    ["MunifTanjim/nui.nvim"] = {
+        module = {"nui.layout", "nui.popup"},
+        module_pattern = {"nui.*"}
+    },
 
     ["jackMort/ChatGPT.nvim"] = {
         opt = true,
+        keys = {"<leader>gpt"},
+        module_pattern = {"chatgpt*"},
+        after = {"nui.nvim", "telescope.nvim"},
+        setup = function()
+            require("custom.plugins.configs.chat-gpt").load_api_key()
+        end,
         config = function()
-            -- require("custom.plugins.configs.chatgpt").setup()
-            require("chatgpt").setup({
-        -- optional configuration
-    })
+            require("custom.plugins.configs.chat-gpt").setup()
         end,
         requires = {
             "MunifTanjim/nui.nvim",
