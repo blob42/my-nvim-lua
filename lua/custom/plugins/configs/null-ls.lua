da3760df12df0e9992eb973ebba4d306c1e944ac
blob42 <spike@w530>
2023-04-29 14:11:38 +0200

    update null-ls

--- lua/custom/plugins/configs/null-ls.lua
+++ lua/custom/plugins/configs/null-ls.lua
@@ -15,16 +15,18 @@ M.default_sources = {
     null_ls.builtins.diagnostics.fish,
     null_ls.builtins.formatting.fish_indent,
     null_ls.builtins.diagnostics.hadolint,
-    null_ls.builtins.formatting.autoflake,
-    null_ls.builtins.formatting.autopep8,
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.isort,
+    null_ls.builtins.formatting.ruff,
+    null_ls.builtins.diagnostics.ruff,
 }
 
 M.extra_sources = {
     null_ls.builtins.diagnostics.jshint,
     null_ls.builtins.diagnostics.flake8,
     null_ls.builtins.diagnostics.pydocstyle,
+    null_ls.builtins.formatting.autoflake,
+    null_ls.builtins.formatting.autopep8,
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.isort,
     -- null_ls.builtins.diagnostics.pylint,
 
 
